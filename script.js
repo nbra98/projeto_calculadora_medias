e338c7fd0e92fbd58b805d4d1bc9ce531aba942c
Luis Fernando <luisfernandobrasilsantos@hotmail.com>
2023-07-31 20:50:04 -0300

    fix: decimal place limitation

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function atualizaTabela() {
 function atualizaMediaFinal() {  
   const mediaFinal = calculaMediaFinal();
 
-  document.querySelector('#media-final-valor').innerHTML = mediaFinal;
+  document.querySelector('#media-final-valor').innerHTML = mediaFinal.toFixed(2);
   document.querySelector('#media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
